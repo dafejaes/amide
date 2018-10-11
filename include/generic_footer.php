98ee6b4459f83cdbc3f435eefd83b31efceb359c
Daniel <daniel.jaramilloe@udea.edu.co>
2018-10-11 18:49:28 -0500

    Formato del pie de página

---
+++ include/generic_footer.php
@@ -0,0 +1,7 @@
+<div class="boxBtnFooter">
+    <a class="BtnFooter" href="#">Preguntas frecuentes</a> I
+    <a class="BtnFooter" href="#">Contenido legal</a> I
+    <a class="BtnFooter" href="#">Contacto</a></div>
+<div id="boxCopyFooter">
+    Copyright AntioquiaMide. Todos los derechos reservados.
+</div>
