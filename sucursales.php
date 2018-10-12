37c8acb8b41174048077594aef0ec4d4a211915b
Daniel <daniel.jaramilloe@udea.edu.co>
2018-10-12 17:19:02 -0500

    Ingreso de archivo javascript pero no funcionó

--- sucursales.php
+++ sucursales.php
@@ -174,6 +174,6 @@ $arrclientes = $arrsucursal['output']['response'];
 <?php include 'include/generic_script.php'; ?>
 <link rel="stylesheet" media="screen" href="../danmet/css/dynamictable.css" type="text/css" />
 <script type="text/javascript" src="js/jquery/jquery-dataTables.js"></script>
-<script type="text/javascript" src="js/clientes.js"></script>
+<script type="text/javascript" src="js/sucursales.js"></script>
 </body>
 </html>
