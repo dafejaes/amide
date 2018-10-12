5ea987f39c4ed7804dd3c6391402c7c98fcbdda8
Daniel <daniel.jaramilloe@udea.edu.co>
2018-10-12 12:23:51 -0500

    Modulos segun permisos

--- include/generic_navbar.php
+++ include/generic_navbar.php
@@ -11,12 +11,12 @@
                 <li class="divider-vertical"></li>
                 <?php
             }
-            if($SESSION_DATA->getPermission(5)){
+            //if($SESSION_DATA->getPermission(5)){
                 ?>
-                <li <?php if ($_ACTIVE_SIDEBAR == "usuario") echo 'class="active"'; ?>><a href="usuario.php">Usuarios</a></li>
-                <li class="divider-vertical"></li>
+                <!--<li <?php //if ($_ACTIVE_SIDEBAR == "usuario") echo 'class="active"'; ?>><a href="usuario.php">Usuarios</a></li>
+                <li class="divider-vertical"></li>-->
                 <?php
-            }
+            //}
             ?>
 
         </ul>
