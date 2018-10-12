42f0aa55a0278a7b0ae41bac87770b384cf51f52
Daniel <daniel.jaramilloe@udea.edu.co>
2018-10-11 23:40:06 -0500

    Cambio del cabazote cuando se loguea

--- include/generic_header.php
+++ include/generic_header.php
@@ -1,3 +1,15 @@
-<div align="center">
+<?php
+
+if (isset($_SESSION['usuario'])) {
+
+}
+else {
+
+?>
+    <div align="center">
     <img src="images/favicon2.png" alt="AntioquiaMide"/>
-</div>
+     </div>
+<?php
+}
+?>
+
