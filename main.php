72052157ad25e1edcfe696c252c15f291f9c7582
Daniel <daniel.jaramilloe@udea.edu.co>
2018-10-11 19:28:32 -0500

    main

---
+++ main.php
@@ -0,0 +1,31 @@
+<?php
+include 'include/generic_validate_session.php';
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+	<?php include 'include/generic_head.php'; ?>
+    </head>
+    <body>
+        <header>
+	    <?php
+	    include 'include/generic_header.php';
+	    ?>
+        </header>
+        <section id="section_wrap">
+            <div class="container">
+		<?php $_ACTIVE_SIDEBAR = ''; include 'include/generic_navbar.php'; ?>
+            </div>
+	    <div class="container" style="height: 300px; margin: 0 auto !important; text-align: center;">
+		<br><br>
+		<h1>BIENVENIDO AL SISTEMA DE INFORMACIÓN DE SECUENCIA24</h1>
+		<?php 
+                //print_r($_SESSION['usuario']) 
+                ?>
+	    </div>	    
+	</section>
+	<footer id="footer_wrap">
+	<?php include 'include/generic_footer.php'; ?>
+	</footer>
+    </body>
+</html>
